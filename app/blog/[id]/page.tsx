e32e6ab1d528e8d276466123742b83f26dd09148
Consoling <shahidarizfannymagnet@gmail.com>
2024-05-30 18:16:30 +0530

    created login page based on role

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -35,21 +35,26 @@ const Blog: React.FC = () => {
       {blog &&
         blog.map((item) => {
           return (
-            <>
-              <div className="w-full flex flex-col justify-center items-start gap-5">
+            <div key={item.id}>
+             <div className="flex flex-row w-full items-center">
+             <div className="w-full flex flex-col justify-center items-start gap-3 my-8">
                 <span className="bg-[#4b6bfb] w-auto text-center h-7 flex items-center justify-center rounded-[5px]  text-[#f6f7ff]  text-xs capitalize leading-relaxed px-5">
                   {/* {item.category} */}Tech
                 </span>
                 <span className="font-bold text-[48px]">{item.title}</span>
                 <span className="font-normal text-xs px-4 gap-2">-{item.verify_by}</span>
               </div>
+             </div>
               <section className="w-full h-[540px] flex-flex-col ">
                 <img
                   className="w-full h-full object-contain"
                   src="/blg-image-1.png"
                 />
               </section>
-            </>
+              <section className="flex flex-row  my-16 w-full">
+                <span className="font-[350] flex flex-wrap">{item.description}</span>
+              </section>
+            </div>
           );
         })}
     </div>
